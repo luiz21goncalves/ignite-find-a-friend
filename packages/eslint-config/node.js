98444db4bd1fe5ce68f597dd64587760d7e4ae70
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-03-03 09:19:57 -0300

    style: update eslint-config to use rocketseat style guide

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -1,9 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals', '@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['simple-import-sort', 'sort-keys'],
   rules: {
-    'react/self-closing-comp': 'error',
+    camelcase: 'off',
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
     'sort-keys': 'off',
