37d480eab495ddf65a85df69795f098f907e075b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-03-03 16:36:47 -0300

    chore(api): add database service and environment variables

--- apps/api/src/env/index.ts
+++ apps/api/src/env/index.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
   PORT: z.coerce.number(),
+  POSTGRESQL_DATABASE: z.string(),
+  POSTGRESQL_HOST: z.string(),
+  POSTGRESQL_PASSWORD: z.string(),
+  POSTGRESQL_PORT: z.coerce.number(),
+  POSTGRESQL_USERNAME: z.string(),
 })
 
 const env = envSchema.safeParse(process.env)
