3e1456d8b11ab8d0122f723983d792533971f9e1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-03-03 09:49:17 -0300

    fix(eslint-config): remove extend next/core-web-vitals

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,9 +1,8 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals', '@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/next'],
   plugins: ['simple-import-sort', 'sort-keys'],
   rules: {
-    'react/self-closing-comp': 'error',
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
     'sort-keys': 'off',
