b46c430a9f57415276c9f0e57eded568d57a042d
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-03-11 19:46:11 -0300

    chore(web): add src folder

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,13 @@
+import './global.css'
+
 import { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className="min-h-screen w-full bg-zinc-50 text-blue-950 antialiased">
+        {children}
+      </body>
     </html>
   )
 }
